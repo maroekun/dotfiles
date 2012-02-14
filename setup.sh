5125b4c243ccd7d300e9637f8f471b1d0a4af22e
maroekun <you05232050@gmail.com>
2012-02-14 15:24:41 +0900

    add setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DOT_FILES=( .zshrc .vimrc .screenrc .bashrc )
+
+for file in ${DOT_FILES[@]}
+do
+  if test -e $HOME/$file ;then
+    echo $file is exist.
+  else
+    ln -s $HOME/dotfiles/$file $HOME/$file
+  fi
+done
