3aa4e3817af9a334435be6591f9f4f77e08d0a96
maroekun <you05232050@gmail.com>
2012-08-10 20:23:32 +0900

    Edit setup.sh, README

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/zsh
 
-DOT_FILES=(.zshrc .vimrc .screenrc .bashrc .tmux.conf .railsrc .gitconfig.local)
+CURRENT=$(cd $(dirname $0); pwd)
 
-CURRENT=`pwd`
+DOT_FILES=(.zshrc .vimrc .screenrc .bashrc .tmux.conf .railsrc .gitconfig.local)
 
 for file in ${DOT_FILES[@]}
 do
@@ -13,3 +13,20 @@ do
     echo $file sym create.
   fi
 done
+
+file_num=`expr $(/bin/ls $CURRENT/dot.vim/vundle.git | wc -l)`
+if [ $file_num -eq 0 ] ; then
+  echo $CURRENT/dot.vim/vundle.git is empty
+  cd $CURRENT
+  git submodule update --init
+else
+  echo $CURRENT/dot.vim/vundle.git already initialize.
+fi
+
+if [ ! -e $HOME/.vim ] ; then
+  mkdir $HOME/.vim
+  echo $HOME/.vim create.
+  ln -s $CURRENT/dot.vim $HOME/.vim/vundle.git
+else
+  echo $HOME/.vim is exist.
+fi
