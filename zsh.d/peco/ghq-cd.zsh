8000397e2ca1264ffa1cac79c9de85759d932f24
maroekun <you05232050@gmail.com>
2017-07-27 23:27:13 +0900

    wip: ghq-list

--- zsh.d/peco/ghq-cd.zsh
+++ zsh.d/peco/ghq-cd.zsh
@@ -1,7 +1,15 @@
 # rquire: ghq command
 function peco-ghq-cd() {
-  local t_dir=$(ghq list | peco)
+  local ghq
+  if [ -x "`which ghq 2>/dev/null`" ]; then
+      ghq=$(which ghq)
+  else
+      echo 'ghq command required'
+      return 1
+  fi
+  echo $ghq
 
+  local t_dir=$(ghq list | peco)
   if [ -n "$t_dir" ] ; then
     BUFFER="cd $(ghq root)/${t_dir}"
     zle accept-line
